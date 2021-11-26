3e8fa8b5406d51a968abb445c5b3e6134769fe8b
yy-dot <52992852+yy-dot@users.noreply.github.com>
2021-11-26 10:09:33 +0800

    Create volcano.R

---
+++ R/volcano.R
@@ -0,0 +1,21 @@
+resdata = read.delim('/home1/yyyu/m6A/ALKBH5_knockdown_RNA/ALKBH5_seq/company_result/3_Differential_result/deglist/siALKBH5vssiCtrl_deg.xls',check.names = F)
+resdata = na.omit(resdata)
+resdata$threshold = factor(ifelse(resdata$padj < 0.05 & abs(resdata$log2FoldChange) >= 1, ifelse(resdata$log2FoldChange>= 1,'Up','Down'),'NoSignifi'))
+
+vol=ggplot(resdata,aes(x=log2FoldChange,y=-log10(padj),color=threshold))+
+  geom_point()+ 
+  scale_color_manual(values=c("#CF0141","#0068B7","#7B7C7D"))+#确定点的颜色#FCD575
+  geom_text_repel(
+    data = resdata[resdata$padj<0.01 & abs(resdata$log2FoldChange)>2,],
+    aes(label = gene_name),
+    size = 3,
+    segment.color = "black", show.legend = FALSE )+ #添加关注的点的基因名
+  theme_bw()+#修改图片背景
+  theme(
+    legend.title = element_blank(),#不显示图例标题
+    plot.title = element_text(hjust = 0.5))+
+  labs(title="siALKBH5 vs siCtrl")+
+  ylab('-log10 p-adj')+#修改y轴名称
+  xlab('log2 FoldChange(Gene Expression)')+#修改x轴名称
+  geom_vline(xintercept=c(-1,1),lty=3,col="black",lwd=0.5) +#添加横线|FoldChange|>2
+  geom_hline(yintercept = -log10(0.05),lty=3,col="black",lwd=0.5)#添加竖线padj<0.05
