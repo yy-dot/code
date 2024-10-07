9473dfc4b244be025cf38fd523ebfc632b2e233a
yy-dot <52992852+yy-dot@users.noreply.github.com>
2024-10-07 21:05:30 +0800

    Update geom_bar.R

--- R/geom_bar.R
+++ R/geom_bar.R
@@ -1,28 +1,28 @@
+#stat = "count"：对于geom_bar()，默认是计算每个x值的行数，不需要提供y值。此时条形高度为x值的行数，即默认值stat = "count"。
+#stat = "identity"：如果设置identity，说明需要提供统计好的y值。此时条形高度为提供的数值。
+#1.position_identity()：不进行任何位置调整，元素按照原始数据的位置进行排列。
+#2.position_stack()：在柱状图和面积图中使用，将元素按照数值堆叠在一起。
+#3.position_fill()：在柱状图和面积图中使用，将元素按照数值进行归一化，填充整个绘图区域。
+#4.position_dodge()：在柱状图和面积图中使用，将元素按照分组进行分开排列，避免重叠。
+#5.position_jitter()：在散点图中使用，对元素的位置进行随机抖动，避免重叠。
+#6.position_nudge()：在散点图和线图中使用，对元素的位置进行微调，使其偏离原始位置。
+
 library(ggplot2)
 library(tidyr)
-
-barplot <- function(anno, xvalue, yvalue, fillvalue, ylable) {
-  p <- ggplot(data=anno) +
-    geom_bar(mapping=aes(x=xvalue, y=yvalue, fill=fillvalue), stat="identity",position=position_dodge(), width = 0.7) + #position_stack(),position_fill()
-    #theme_classic() +
-    theme(panel.background = element_rect(fill = NA),
-          axis.line = element_line(colour = "black"),
-          axis.text.x = element_text(angle = 45, vjust = 1),
-          axis.text.y = element_text(size = 12, margin=margin(0,0,0,10)),
-          axis.title.x=element_blank(), 
-          axis.title.y = element_text(size=12),
-          axis.line.x = element_blank(),
-          axis.ticks.x = element_blank()) +
-    labs(y= ylable) +
-    scale_fill_manual(values=c("#EB6101","#94ADDA", "#FCD575", "#A69425", "#9078B6", "#B4766B")) +
-    geom_hline(aes(yintercept=0))
-  return(p)
-}
-
-
 anno <- gather(raw.data,sample,values,-annotation)
 anno$sample <-factor(anno$sample,level=anno$sample)
-p <- barplot(anno=anno, xvalue=anno$sample, yvalue=anno$values, fillvalue=anno$annotation, ylable='Ratio of observed to expected peaks(log2)')
+
+barplot.p <- ggplot(data=anno) +
+  geom_bar(mapping=aes(x=xvalue, y=yvalue, fill=fillvalue), stat="identity",position=position_dodge(), width = 0.7) + 
+  theme(panel.background = element_rect(fill = NA),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1,size = 12,color="black"),
+        axis.text.y = element_text(size = 12, margin=margin(0,0,0,10),color="black"),
+        axis.title.x = element_blank(), 
+        axis.title.y = element_text(size=14)) +
+  labs(x= xlable, y=ylable, title=title) +
+  scale_fill_manual(values=c("#EB6101","#94ADDA", "#FCD575", "#A69425", "#9078B6", "#B4766B")) +
+  scale_y_continuous(expand = c(0, 0))#使bar图紧贴x轴 #geom_hline(aes(yintercept=0))#加入y=0的横线
 
 
 
