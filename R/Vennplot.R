4b270f71e42de8a6a35a18a5d84575a0004e51d8
yy-dot <52992852+yy-dot@users.noreply.github.com>
2024-11-05 09:40:47 +0800

    Update Vennplot.R

--- R/Vennplot.R
+++ R/Vennplot.R
@@ -1,3 +1,4 @@
+#第一种
 library(grid)
 library(VennDiagram)
 zeroone <- read.delim("/home1/yyyu/Embryo/H3K9ac/3.0macs2/com_peak_matrix/all_zeroone.matirx",header =T,sep = "\t",stringsAsFactors = F,check.names = FALSE)
@@ -9,3 +10,34 @@ pdf("./important_plot/Venn_peaks_shared_by_ESC_TSC.pdf",width = 6,height = 6)
 grid.newpage()
 grid.draw(D1)
 dev.off()
+
+#第二种
+library(eulerr)
+library(dplyr)
+
+PN5_integrat_01=read.delim('PN5_integrat_01.bed',header = F) %>% dplyr::mutate(flag=row_number())
+colnames(PN5_integrat_01)=c("chr","start","end","k9ac","k4me3","k27ac","flag")
+
+PN5_VS=list("k9ac"=PN5_integrat_01[PN5_integrat_01$k9ac==1,c('flag')], 
+           "k4me3"=PN5_integrat_01[PN5_integrat_01$k4me3==1,c('flag')], 
+           "k27ac"=PN5_integrat_01[PN5_integrat_01$k27ac==1,c('flag')])
+
+PN5_partion <- get.venn.partitions(PN5_VS)
+PN5_combinations=c(
+  "k9ac"=PN5_partion[PN5_partion["..set.."]=="(k9ac)∖(k4me3∪k27ac)","..count.."],
+  "k4me3"=PN5_partion[PN5_partion["..set.."]=="(k4me3)∖(k9ac∪k27ac)","..count.."],
+  "k27ac"=PN5_partion[PN5_partion["..set.."]=="(k27ac)∖(k9ac∪k4me3)","..count.."],
+  "k9ac&k4me3"=PN5_partion[PN5_partion["..set.."]=="(k9ac∩k4me3)∖(k27ac)","..count.."],
+  "k9ac&k27ac"=PN5_partion[PN5_partion["..set.."]=="(k9ac∩k27ac)∖(k4me3)","..count.."],
+  "k4me3&k27ac"=PN5_partion[PN5_partion["..set.."]=="(k4me3∩k27ac)∖(k9ac)","..count.."],
+  "k9ac&k4me3&k27ac"=PN5_partion[PN5_partion["..set.."]=="k9ac∩k4me3∩k27ac","..count.."]
+)
+
+PN5_euler=euler(PN5_combinations)
+p_PN5=plot(PN5_euler, 
+          quantities =list(type =c("counts")),
+          fills=list(fill=c('#CE0535','#0068B7',"#FCD575"),alpha=0.5),
+          edges = list(col="black",lwd=2),
+          main = list(label=c("PN5 peaks shared by k9ac_k4me3_k27ac"),cex=1))
+
+p_PN5
